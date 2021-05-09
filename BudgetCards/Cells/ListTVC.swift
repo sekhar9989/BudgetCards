218422d19eb7fe99acbdf074550079d6baf003c9
Sekhar Simhadri <simhadri.somasekhar@mobileprogramming.com>
2021-05-09 18:32:57 +0530

    Testing Commit

--- BudgetCards/Cells/ListTVC.swift
+++ BudgetCards/Cells/ListTVC.swift
@@ -7,15 +7,6 @@ class ListTVC: UITableViewCell {
     @IBOutlet var lblAmount : UILabel!
     @IBOutlet weak var lblDate: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    //nothing
 
 }
